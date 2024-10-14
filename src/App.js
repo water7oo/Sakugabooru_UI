e56aa2dfb0d21d079e0cc402513f19a250d94177
water7oo <watersevens37@gmail.com>
2024-10-14 16:28:52 -0400

    Added Carousel Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TestStuff from "./bgCarousel";
 
 function Home() {
   return (
     <div id="master">
+      <TestStuff />
+      <div id="logoTitle">
+        <div id="logoContainer">
+          <img src="\images\Sakugabooru1(whitefill).png" alt="booruText"></img>
+        </div>
+        <span></span>
+        <p>
+          SAKUGA<span class="highlight">BOORU</span>
+        </p>
+      </div>
+
       <div id="miniNav">
         <button>Posts</button>
         <span></span>
@@ -15,17 +26,9 @@ function Home() {
         <span></span>
         <button>test5</button>
       </div>
-      <div id="logoTitle">
-        <div id="logoContainer">
-          <img src="\images\Sakugabooru1(whitefill).png" alt="booruText"></img>
-        </div>
-        <span></span>
-        <p>
-          SAKUGA<span class="highlight">BOORU</span>
-        </p>
-      </div>
       <div id="searchBar">
         <input type="text" placeholder="Search..."></input>
+        <span></span>
         <button> X </button>
       </div>
       <div id="searchBtn">
@@ -37,9 +40,21 @@ function Home() {
         </div>
         <div id="tags">
           <button id="tagElements">kanada_flare</button>
+          <span></span>
           <button id="tagElements">yutapon_cubes</button>
+          <span></span>
           <button id="tagElements">umikoshi_eye</button>
+          <span></span>
           <button id="tagElements">smears</button>
+          <span></span>
+          <button id="tagElements">character_acting</button>
+          <span></span>
+          <button id="tagElements">character_acting</button>
+          <span></span>
+          <button id="tagElements">character_acting</button>
+          <span></span>
+          <button id="tagElements">character_acting</button>
+          <span></span>
           <button id="tagElements">character_acting</button>
         </div>
       </div>
---
+++ src/bgCarousel.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./bgCarousel.css";
+
+function testStuff() {
+  return (
+    <div id="testingCSS">
+      <img class="clipped-image" id="makimaIMG" src="\images\Makima.png"></img>
+    </div>
+  );
+}
+
+export default testStuff;
